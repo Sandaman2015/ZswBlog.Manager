28e42a5ad55c208312c2486842431ffad60ea166
sandaman2015@163.com <sandaman2015@163.com>
2020-12-01 21:51:14 +0800

    ZswBlog后端启动

---
+++ src/views/article/article-edit.vue
@@ -0,0 +1,15 @@
+<template>
+    <div style="padding:30px;">
+      <el-alert :closable="false" title="menu 2" />
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
