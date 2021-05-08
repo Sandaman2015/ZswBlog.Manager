dfa27e44f1e68f39be526356bb3d7342a4af3226
sandaman2015 <sandaman2015@163.com>
2021-05-08 17:52:56 +0800

    分类对接

--- src/views/message/message-list.vue
+++ src/views/message/message-list.vue
@@ -1,24 +1,33 @@
 <template>
     <div style="padding: 30px">
-    <el-table
-    :data="tableData"
-    border
-    style="width: 100%">
-    <el-table-column
-      prop="date"
-      label="日期"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="name"
-      label="姓名"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="address"
-      label="地址">
-    </el-table-column>
-  </el-table>
+    <el-table :data="categoryList" border style="width: 100%">
+      <el-table-column prop="id" label="编码" width="180"> </el-table-column>
+      <el-table-column prop="name" label="标题" width="180"> </el-table-column>
+      <el-table-column prop="description" label="副标题" width="180"> </el-table-column>
+      <el-table-column prop="articleCount" label="文章数量" width="180"> </el-table-column>
+       <el-table-column label="创建时间">
+        <template slot-scope="scope">
+          <i class="el-icon-time"></i>
+          <span style="margin-left: 10px">{{
+            scope.row.createDate | timeFilter
+          }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button @click="categoryEdit(scope.row)" type="primary" size="small"
+            >编辑</el-button
+          >
+          <el-button @click="removeCategoryById(scope.row)" type="danger" size="small"
+            >删除</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+    <edit-box :categoryId="isShow.showId" @showDialog="disabledEditDialogVisibility"
+      :_centerDialogVisible="isShow.showEdit" />
+    <save-box :categoryId="isShow.showId" @showDialog="disabledSaveDialogVisibility"
+      :_centerDialogVisible="isShow.showSave" />
 </div>
 </template>
 
